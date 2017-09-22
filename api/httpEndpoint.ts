d600a58a15571e6ff39f70674b0b3265d8faa764
Luke Hoban <luke@pulumi.com>
2017-09-22 16:52:18 -0700

    Add Topic<T> doc comments
    Also apply more consistent doc line wrapping.

--- api/httpEndpoint.ts
+++ api/httpEndpoint.ts
@@ -15,8 +15,8 @@ export interface Request {
      */
     method: string;
     /**
-     * The path parameters of the HTTP request. Each `{param}` in the matched route is available as a
-     * property of this oject.
+     * The path parameters of the HTTP request. Each `{param}` in the matched
+     * route is available as a property of this oject.
      */
     params: { [param: string]: string; };
     /**
@@ -38,23 +38,28 @@ export interface Request {
  */
 export interface Response {
     /**
-     * Sets the HTTP response status code and returns a `Response` for chaining operations.
+     * Sets the HTTP response status code and returns a `Response` for chaining
+     * operations.
      */
     status(code: number): Response;
     /**
-     * Sets a header on the HTTP response and returns the `Response` for chaining operations.
+     * Sets a header on the HTTP response and returns the `Response` for
+     * chaining operations.
      */
     setHeader(name: string, value: string): Response;
     /**
-     * Writes a string to the HTTP response body and returns the `Response` for chaining operations.
+     * Writes a string to the HTTP response body and returns the `Response` for
+     * chaining operations.
      */
     write(data: string): Response;
     /**
-     * Sends the HTTP response, optionally including data to write to the HTTP response body.
+     * Sends the HTTP response, optionally including data to write to the HTTP
+     * response body.
      */
     end(data?: string): void;
     /**
-     * JSON serializes an object, writes it to the HTTP response body, and sends the HTTP response.
+     * JSON serializes an object, writes it to the HTTP response body, and sends
+     * the HTTP response.
      */
     json(obj: any): void;
 }
@@ -62,8 +67,9 @@ export interface Response {
 /**
  * RouteHandler represents a handler for a route on an HttpEndpoint.
  *
- * Implementations should invoke methods on `res` to respond to the request, or invoke `next`
- * to pass control to the next available handler on the route for further processing.
+ * Implementations should invoke methods on `res` to respond to the request, or
+ * invoke `next` to pass control to the next available handler on the route for
+ * further processing.
  */
 export type RouteHandler = (req: Request, res: Response, next: () => void) => void;
 
@@ -74,7 +80,8 @@ export interface HttpEndpointConstructor {
 export let HttpEndpoint: HttpEndpointConstructor; // tslint:disable-line
 
 /**
- * HttpEndpoint publishes an internet-facing HTTP API, for serving web applications or REST APIs.
+ * HttpEndpoint publishes an internet-facing HTTP API, for serving web
+ * applications or REST APIs.
  *
  * ```javascript
  * let api = new HttpEndpoint("myapi")
@@ -85,30 +92,36 @@ export let HttpEndpoint: HttpEndpointConstructor; // tslint:disable-line
  * );
  * ```
  *
- * Paths are `/` seperated.  A path can use `{param}` to capture zero-or-more non-`/` characters
- * and make the captured path segment available in `req.params.param`, or `{param+}` to greedily
- * capture all remaining characters in the url path into `req.params.param`.
+ * Paths are `/` seperated.  A path can use `{param}` to capture zero-or-more
+ * non-`/` characters and make the captured path segment available in
+ * `req.params.param`, or `{param+}` to greedily capture all remaining
+ * characters in the url path into `req.params.param`.
  *
- * Paths and routing are defined statically, and cannot overlap. Code inside a route handler
- * can be used to provide dynamic decisions about sub-routing within a static path.
+ * Paths and routing are defined statically, and cannot overlap. Code inside a
+ * route handler can be used to provide dynamic decisions about sub-routing
+ * within a static path.
  */
 export interface HttpEndpoint {
     /**
-     * The url that the HttpEndpoint is being served at. Set only after a succesful call to `publish`.
+     * The url that the HttpEndpoint is being served at. Set only after a
+     * succesful call to `publish`.
      */
     url?: pulumi.Computed<string>;
 
     /**
-     * staticFile serves a static file from within the source folder at the requested path.
+     * staticFile serves a static file from within the source folder at the
+     * requested path.
      *
      * @param path The route path at which to serve the file.
-     * @param filePath The local file path relative to the Pulumi program folder.
+     * @param filePath The local file path relative to the Pulumi program
+     * folder.
      * @param contentType The `content-type` to serve the file as.
      */
     staticFile(path: string, filePath: string, contentType?: string): void;
 
     /**
-     * Routes any requests with given HTTP method on the given path to the provided handler(s).
+     * Routes any requests with given HTTP method on the given path to the
+     * provided handler(s).
      * @param method The HTTP method to handle.
      * @param path The path to handle requests on.
      * @param handlers One or more handlers to apply to requests.
@@ -162,25 +175,30 @@ export interface HttpEndpoint {
      *
      * This should be called after describing desired routes.
      *
-     * @returns A computed string representing the URL at which the HttpEndpoint is available to the internet.
+     * @returns A computed string representing the URL at which the HttpEndpoint
+     * is available to the internet.
      */
     publish(): pulumi.Computed<string>;
 
     /**
      * Attach a custom domain to this HttpEndpoint.
      *
-     * Provide a domain name you own, along with SSL certificates from a certificate authority (e.g. LetsEncrypt).
-     * The return value is a domain name that you must map your custom domain to using a DNS A record.
+     * Provide a domain name you own, along with SSL certificates from a
+     * certificate authority (e.g. LetsEncrypt). The return value is a domain
+     * name that you must map your custom domain to using a DNS A record.
      *
-     * _Note_: It is strongly encouraged to store certificates in config variables and not in source code.
+     * _Note_: It is strongly encouraged to store certificates in config
+     * variables and not in source code.
      *
-     * @returns The domain name that you must map your custom domain to using a DNS A record.
+     * @returns The domain name that you must map your custom domain to using a
+     * DNS A record.
      */
     attachCustomDomain(domain: Domain): pulumi.Computed<string>;
 }
 
 /**
- * Domain includes the domain name and certificate data to enable hosting an HttpEndpoint on a custom domain.
+ * Domain includes the domain name and certificate data to enable hosting an
+ * HttpEndpoint on a custom domain.
  */
 export interface Domain {
     /**
@@ -196,7 +214,8 @@ export interface Domain {
      */
     certificatePrivateKey: string;
     /**
-     * The certificate chain for the SSL/TLS certificate provided for this domain (`chain.pem`).
+     * The certificate chain for the SSL/TLS certificate provided for this
+     * domain (`chain.pem`).
      */
     certificateChain: string;
 }
