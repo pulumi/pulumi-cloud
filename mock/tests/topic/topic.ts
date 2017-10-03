ba642174a465b7d5768cb26d179d302366d671c6
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-10-03 12:20:07 -0700

    Add simple pub/sub test.

--- mock/tests/topic/topic.ts
+++ mock/tests/topic/topic.ts
@@ -6,6 +6,7 @@ pulumi.runtime.setConfig("cloud:config:provider", "mock");
 import * as cloud from "@pulumi/cloud";
 import * as assert from "assert";
 import * as chai from "chai";
+// import { awaitOutstandingAsynchronousRequests } from "../../topic";
 
 declare module "assert" {
     function throwsAsync(body: () => Promise<void>): Promise<void>;
@@ -24,95 +25,28 @@ declare module "assert" {
     throw new Error("Expected error to be thrown");
 };
 
-describe("Table", () => {
+describe("Topic", () => {
     let uniqueId = 0;
 
-    describe("#get()", () => {
-        it("should-throw-with-no-primary-key", async () => {
-            let table = new cloud.Table("table" + uniqueId++);
-            await assert.throwsAsync(async () => await table.get({}));
-        });
+    describe("#subscribe()", () => {
+        it("should-see-published-value", async () => {
+            let topic = new cloud.Topic<number>("topic" + uniqueId++);
 
-        it("should-throw-with-primary-key-not-present", async () => {
-            let table = new cloud.Table("table" + uniqueId++);
-            await assert.throwsAsync(async () => await table.get({[table.primaryKey]: "val"}));
-        });
+            let numbers: number[] = [];
+            let resolve: () => void;
+            let promise = new Promise((res, reject) => resolve = res);
 
-        it("should-find-inserted-value", async () => {
-            let table = new cloud.Table("table" + uniqueId++);
-            await table.insert({[table.primaryKey]: "val", value: 1});
-            assert.equal((await table.get({[table.primaryKey]: "val"})).value, 1);
-        });
+            topic.subscribe("", num => {
+                numbers.push(num);
+                resolve();
+                return Promise.resolve();
+            });
 
-        it("should-not-be-affected-by-query-data", async () => {
-            let table = new cloud.Table("table" + uniqueId++);
-            await table.insert({[table.primaryKey]: "val", value: 1});
-            assert.equal((await table.get({[table.primaryKey]: "val", value: 2})).value, 1);
-        });
+            topic.publish(10);
+            await promise;
 
-        it("should-see-second insert", async () => {
-            let table = new cloud.Table("table" + uniqueId++);
-            await table.insert({[table.primaryKey]: "val", value: 1});
-            await table.insert({[table.primaryKey]: "val", value: 2});
-            assert.equal((await table.get({[table.primaryKey]: "val", value: 3})).value, 2);
-        });
-
-        it("should-not-see-deleted-value", async () => {
-            let table = new cloud.Table("table" + uniqueId++);
-            await table.insert({[table.primaryKey]: "val", value: 1});
-            await table.delete({[table.primaryKey]: "val" });
-            await assert.throwsAsync(async () => await table.get({[table.primaryKey]: "val"}));
-        });
-
-        it("should-not-see-inserts-to-other-table", async () => {
-            let table1 = new cloud.Table("table" + uniqueId++);
-            let table2 = new cloud.Table("table" + uniqueId++);
-
-            await table1.insert({[table1.primaryKey]: "val", value: 1});
-            await assert.throwsAsync(async () => await table2.get({[table2.primaryKey]: "val"}));
-        });
-    });
-
-    describe("#update()", () => {
-        it("should-only-update-provided-keys", async () => {
-            let table = new cloud.Table("table" + uniqueId++);
-            await table.insert({[table.primaryKey]: "val", value1: 1, value2: "2"});
-            await table.update({[table.primaryKey]: "val" }, {value1: 3});
-
-            assert.equal((await table.get({[table.primaryKey]: "val"})).value1, 3);
-            assert.equal((await table.get({[table.primaryKey]: "val"})).value2, "2");
-        });
-    });
-
-    describe("#scan()", () => {
-        it("returns-all-values", async () => {
-            let table = new cloud.Table("table" + uniqueId++);
-            await table.insert({[table.primaryKey]: "val1", value1: 1, value2: "1"});
-            await table.insert({[table.primaryKey]: "val2", value1: 2, value2: "2"});
-
-            let values = await table.scan();
-            assert.equal(values.length, 2);
-
-            let value1 = values.find(v => v[table.primaryKey] === "val1");
-            let value2 = values.find(v => v[table.primaryKey] === "val2");
-
-            assert.notEqual(value1, value2);
-            assert.equal(value1.value1, 1);
-            assert.equal(value2.value1, 2);
-        });
-
-        it("does-not-return-deleted-value", async () => {
-            let table = new cloud.Table("table" + uniqueId++);
-            await table.insert({[table.primaryKey]: "val1", value1: 1, value2: "1"});
-            await table.insert({[table.primaryKey]: "val2", value1: 2, value2: "2"});
-            await table.delete({[table.primaryKey]: "val1"});
-
-            let values = await table.scan();
-            assert.equal(values.length, 1);
-
-            let value = values[0];
-
-            assert.equal(value.value1, 2);
+            assert.equal(1, numbers.length);
+            assert.equal(10, numbers[0]);
         });
     });
 });
--- mock/topic.ts
+++ mock/topic.ts
@@ -12,6 +12,11 @@ export class Topic<T> implements cloud.Topic<T> {
         const subscribers = globalTopicToSubscribers[name] || (globalTopicToSubscribers[name] = []);
 
         this.publish = item => {
+            // For testing purposes we have to keep track that we've started an async operation.
+            // This way the test can have a way of waiting until the actual notification to the
+            // subscriber has been done before validating anything.
+            // onAsynchronousOperationStarted();
+
             // Make publishing seem asynchronous by actually doing the notification on the next
             // event loop.
             setTimeout(
@@ -19,6 +24,8 @@ export class Topic<T> implements cloud.Topic<T> {
                     for (const subscriber of subscribers) {
                         subscriber(item);
                     }
+
+                    // onAsynchronousOperationFinished();
                 },
                 1);
             return Promise.resolve();
@@ -29,3 +36,48 @@ export class Topic<T> implements cloud.Topic<T> {
         };
     }
 }
+
+// let outstandingAsynchronousOperationCount = 0;
+// let outstandingAsynchronousOperationListeners: { (): void }[] = [];
+
+// function onAsynchronousOperationStarted() {
+//     console.log("Entering onAsynchronousOperationStarted");
+//     if (outstandingAsynchronousOperationCount < 0) {
+//         throw new Error("outstandingAsynchronousRequestsCount < 0");
+//     }
+
+//     outstandingAsynchronousOperationCount++;
+//     console.log("outstandingAsynchronousOperationCount=" + outstandingAsynchronousOperationCount);
+// }
+
+// function onAsynchronousOperationFinished() {
+//     console.log("Entering onAsynchronousOperationFinished");
+//     outstandingAsynchronousOperationCount--;
+//     console.log("outstandingAsynchronousOperationCount=" + outstandingAsynchronousOperationCount);
+//     if (outstandingAsynchronousOperationCount < 0) {
+//         throw new Error("outstandingAsynchronousRequests < 0");
+//     }
+
+//     if (outstandingAsynchronousOperationCount === 0) {
+//         const listeners = outstandingAsynchronousOperationListeners;
+//         outstandingAsynchronousOperationListeners = [];
+
+//         for (const listener of listeners) {
+//             listener();
+//         }
+//     }
+// }
+
+// export function awaitOutstandingAsynchronousRequests(): Promise<void> {
+//     console.log("Entering awaitOutstandingAsynchronousRequests");
+//     if (outstandingAsynchronousOperationCount === 0) {
+//         console.log("No outstanding requests");
+//         // no actual outstanding async requests.  Caller doesn't need to wait for anything.
+//         return Promise.resolve();
+//     }
+
+//     console.log("Outstanding requests");
+//     return new Promise((resolve, reject) => {
+//         outstandingAsynchronousOperationListeners.push(resolve);
+//     });
+// }
