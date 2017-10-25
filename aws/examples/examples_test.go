39187b54a9175f99d6556a66c207812aa6dc6844
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-10-25 16:54:35 -0700

    restore tests.

--- aws/examples/examples_test.go
+++ aws/examples/examples_test.go
@@ -29,26 +29,26 @@ func Test_Examples(t *testing.T) {
 		return
 	}
 	examples := []integration.ProgramTestOptions{
-		// {
-		// 	Dir: path.Join(cwd, "../../examples/crawler"),
-		// 	Config: map[string]string{
-		// 		"aws:config:region":     region,
-		// 		"cloud:config:provider": "aws",
-		// 	},
-		// 	Dependencies: []string{
-		// 		"@pulumi/cloud",
-		// 		"@pulumi/cloud-aws",
-		// 	},
-		// },
-		// {
-		// 	Dir: path.Join(cwd, "../../examples/countdown"),
-		// 	Config: map[string]string{
-		// 		"aws:config:region": region,
-		// 	},
-		// 	Dependencies: []string{
-		// 		"@pulumi/cloud",
-		// 	},
-		// },
+		{
+			Dir: path.Join(cwd, "../../examples/crawler"),
+			Config: map[string]string{
+				"aws:config:region":     region,
+				"cloud:config:provider": "aws",
+			},
+			Dependencies: []string{
+				"@pulumi/cloud",
+				"@pulumi/cloud-aws",
+			},
+		},
+		{
+			Dir: path.Join(cwd, "../../examples/countdown"),
+			Config: map[string]string{
+				"aws:config:region": region,
+			},
+			Dependencies: []string{
+				"@pulumi/cloud",
+			},
+		},
 		{
 			Dir: path.Join(cwd, "../../examples/todo"),
 
@@ -125,19 +125,19 @@ func Test_Examples(t *testing.T) {
 				t.Logf("GET %v [%v]: %v", baseURL+"/todo", resp.StatusCode, string(bytes))
 			},
 		},
-		// {
-		// 	Dir: path.Join(cwd, "../../examples/timers"),
-		// 	Config: map[string]string{
-		// 		"aws:config:region":     region,
-		// 		"cloud:config:provider": "aws",
-		// 		"timers:config:message": "Hello, Pulumi Timers!",
-		// 	},
-		// 	Dependencies: []string{
-		// 		"pulumi",
-		// 		"@pulumi/cloud",
-		// 		"@pulumi/cloud-aws",
-		// 	},
-		// },
+		{
+			Dir: path.Join(cwd, "../../examples/timers"),
+			Config: map[string]string{
+				"aws:config:region":     region,
+				"cloud:config:provider": "aws",
+				"timers:config:message": "Hello, Pulumi Timers!",
+			},
+			Dependencies: []string{
+				"pulumi",
+				"@pulumi/cloud",
+				"@pulumi/cloud-aws",
+			},
+		},
 		// Leaving out of integration tests until we have shareable credentials for testing these integrations.
 	}
 	for _, ex := range examples {
