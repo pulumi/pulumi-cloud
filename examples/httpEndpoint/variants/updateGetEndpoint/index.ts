ca2155e8777309c64d10d9390592e6de5d60d66e
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-10-26 18:16:26 -0700

    Add test to validate update behavior.

--- examples/httpEndpoint/variants/updateGetEndpoint/index.ts
+++ examples/httpEndpoint/variants/updateGetEndpoint/index.ts
@@ -3,9 +3,9 @@
 import * as cloud from "@pulumi/cloud";
 
 const endpoint = new cloud.HttpEndpoint("test");
-endpoint.get("/test1.txt", (req, res) => {
+endpoint.get("/test2.txt", (req, res) => {
     res.setHeader("Content-Type", "text/html");
-    res.end("You got test1");
+    res.end("You got test2");
 });
 
 endpoint.publish();
