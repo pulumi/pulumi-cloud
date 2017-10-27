fc4529b3e315cf358b396973532b0399d0108d64
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-10-26 18:20:55 -0700

    REstore tests.

--- aws/examples/examples_test.go
+++ aws/examples/examples_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -28,129 +29,129 @@ func Test_Examples(t *testing.T) {
 		return
 	}
 	examples := []integration.ProgramTestOptions{
-		// {
-		// 	Dir: path.Join(cwd, "../../examples/crawler"),
-		// 	Config: map[string]string{
-		// 		"aws:config:region":     region,
-		// 		"cloud:config:provider": "aws",
-		// 	},
-		// 	Dependencies: []string{
-		// 		"@pulumi/cloud",
-		// 		"@pulumi/cloud-aws",
-		// 	},
-		// },
-		// {
-		// 	Dir: path.Join(cwd, "../../examples/countdown"),
-		// 	Config: map[string]string{
-		// 		"aws:config:region": region,
-		// 		// TODO[pulumi/pulumi-cloud#138]: Would love to use this example to test private networking for
-		// 		// lambdas, but we are blocked on doing this in CI due to the inability to automatically delete
-		// 		// the VPC used for hosting Lambda within a day of running a Lambda in it.
-		// 		// "cloud-aws:config:usePrivateNetwork": "true",
-		// 	},
-		// 	Dependencies: []string{
-		// 		"@pulumi/cloud",
-		// 	},
-		// },
-		// {
-		// 	Dir: path.Join(cwd, "../../examples/containers"),
-		// 	Config: map[string]string{
-		// 		"aws:config:region":               region,
-		// 		"cloud-aws:config:ecsAutoCluster": "true",
-		// 	},
-		// 	Dependencies: []string{
-		// 		"@pulumi/cloud",
-		// 	},
-		// },
-		// {
-		// 	Dir: path.Join(cwd, "../../examples/todo"),
+		{
+			Dir: path.Join(cwd, "../../examples/crawler"),
+			Config: map[string]string{
+				"aws:config:region":     region,
+				"cloud:config:provider": "aws",
+			},
+			Dependencies: []string{
+				"@pulumi/cloud",
+				"@pulumi/cloud-aws",
+			},
+		},
+		{
+			Dir: path.Join(cwd, "../../examples/countdown"),
+			Config: map[string]string{
+				"aws:config:region": region,
+				// TODO[pulumi/pulumi-cloud#138]: Would love to use this example to test private networking for
+				// lambdas, but we are blocked on doing this in CI due to the inability to automatically delete
+				// the VPC used for hosting Lambda within a day of running a Lambda in it.
+				// "cloud-aws:config:usePrivateNetwork": "true",
+			},
+			Dependencies: []string{
+				"@pulumi/cloud",
+			},
+		},
+		{
+			Dir: path.Join(cwd, "../../examples/containers"),
+			Config: map[string]string{
+				"aws:config:region":               region,
+				"cloud-aws:config:ecsAutoCluster": "true",
+			},
+			Dependencies: []string{
+				"@pulumi/cloud",
+			},
+		},
+		{
+			Dir: path.Join(cwd, "../../examples/todo"),
 
-		// 	Config: map[string]string{
-		// 		"aws:config:region":     region,
-		// 		"cloud:config:provider": "aws",
-		// 	},
-		// 	Dependencies: []string{
-		// 		"@pulumi/cloud",
-		// 		"@pulumi/cloud-aws",
-		// 	},
-		// 	ExtraRuntimeValidation: func(t *testing.T, checkpoint stack.Checkpoint) {
-		// 		_, _, snapshot, err := stack.DeserializeCheckpoint(&checkpoint)
-		// 		if !assert.Nil(t, err, "expected checkpoint deserialization to succeed") {
-		// 			return
-		// 		}
-		// 		pulumiResources := pulumiframework.GetComponents(snapshot.Resources)
-		// 		urn := resource.NewURN(checkpoint.Target, "todo", "pulumi:framework:Endpoint", "todo")
-		// 		endpoint := pulumiResources[urn]
-		// 		if !assert.NotNil(t, endpoint, "expected to find endpoint") {
-		// 			return
-		// 		}
-		// 		baseURL := endpoint.Properties["url"].StringValue()
-		// 		assert.NotEmpty(t, baseURL, "expected a `todo` endpoint")
+			Config: map[string]string{
+				"aws:config:region":     region,
+				"cloud:config:provider": "aws",
+			},
+			Dependencies: []string{
+				"@pulumi/cloud",
+				"@pulumi/cloud-aws",
+			},
+			ExtraRuntimeValidation: func(t *testing.T, checkpoint stack.Checkpoint) {
+				_, _, snapshot, err := stack.DeserializeCheckpoint(&checkpoint)
+				if !assert.Nil(t, err, "expected checkpoint deserialization to succeed") {
+					return
+				}
+				pulumiResources := pulumiframework.GetComponents(snapshot.Resources)
+				urn := resource.NewURN(checkpoint.Target, "todo", "pulumi:framework:Endpoint", "todo")
+				endpoint := pulumiResources[urn]
+				if !assert.NotNil(t, endpoint, "expected to find endpoint") {
+					return
+				}
+				baseURL := endpoint.Properties["url"].StringValue()
+				assert.NotEmpty(t, baseURL, "expected a `todo` endpoint")
 
-		// 		// Validate the GET / endpoint
-		// 		resp, err := http.Get(baseURL)
-		// 		assert.NoError(t, err, "expected to be able to GET /")
-		// 		contentType := resp.Header.Get("Content-Type")
-		// 		assert.Equal(t, "text/html", contentType)
-		// 		bytes, err := ioutil.ReadAll(resp.Body)
-		// 		assert.NoError(t, err)
-		// 		t.Logf("GET %v [%v/%v]: %v", baseURL, resp.StatusCode, contentType, string(bytes))
+				// Validate the GET / endpoint
+				resp, err := http.Get(baseURL)
+				assert.NoError(t, err, "expected to be able to GET /")
+				contentType := resp.Header.Get("Content-Type")
+				assert.Equal(t, "text/html", contentType)
+				bytes, err := ioutil.ReadAll(resp.Body)
+				assert.NoError(t, err)
+				t.Logf("GET %v [%v/%v]: %v", baseURL, resp.StatusCode, contentType, string(bytes))
 
-		// 		// Validate the GET /index.html endpoint
-		// 		resp, err = http.Get(baseURL + "/index.html")
-		// 		assert.NoError(t, err, "expected to be able to GET /index.html")
-		// 		contentType = resp.Header.Get("Content-Type")
-		// 		assert.Equal(t, "text/html", contentType)
-		// 		bytes, err = ioutil.ReadAll(resp.Body)
-		// 		assert.NoError(t, err)
-		// 		t.Logf("GET %v [%v/%v]: %v", baseURL, resp.StatusCode, contentType, string(bytes))
+				// Validate the GET /index.html endpoint
+				resp, err = http.Get(baseURL + "/index.html")
+				assert.NoError(t, err, "expected to be able to GET /index.html")
+				contentType = resp.Header.Get("Content-Type")
+				assert.Equal(t, "text/html", contentType)
+				bytes, err = ioutil.ReadAll(resp.Body)
+				assert.NoError(t, err)
+				t.Logf("GET %v [%v/%v]: %v", baseURL, resp.StatusCode, contentType, string(bytes))
 
-		// 		// Validate the GET /favico.ico endpoint
-		// 		resp, err = http.Get(baseURL + "/favicon.ico")
-		// 		assert.NoError(t, err, "expected to be able to GET /favicon.ico")
-		// 		assert.Equal(t, int64(1150), resp.ContentLength)
-		// 		contentType = resp.Header.Get("Content-Type")
-		// 		assert.Equal(t, "image/x-icon", contentType)
-		// 		t.Logf("GET %v [%v]: ...", baseURL+"/favicon.ico", resp.StatusCode)
+				// Validate the GET /favico.ico endpoint
+				resp, err = http.Get(baseURL + "/favicon.ico")
+				assert.NoError(t, err, "expected to be able to GET /favicon.ico")
+				assert.Equal(t, int64(1150), resp.ContentLength)
+				contentType = resp.Header.Get("Content-Type")
+				assert.Equal(t, "image/x-icon", contentType)
+				t.Logf("GET %v [%v]: ...", baseURL+"/favicon.ico", resp.StatusCode)
 
-		// 		// Validate the POST /todo/{id} endpoint
-		// 		resp, err = http.Post(baseURL+"/todo/abc",
-		// 			"application/x-www-form-urlencoded", strings.NewReader("xyz"))
-		// 		assert.NoError(t, err, "expected to be able to POST /todo/{id}")
-		// 		assert.Equal(t, 201, resp.StatusCode)
-		// 		t.Logf("POST %v [%v]: ...", baseURL+"/todo/abc", resp.StatusCode)
+				// Validate the POST /todo/{id} endpoint
+				resp, err = http.Post(baseURL+"/todo/abc",
+					"application/x-www-form-urlencoded", strings.NewReader("xyz"))
+				assert.NoError(t, err, "expected to be able to POST /todo/{id}")
+				assert.Equal(t, 201, resp.StatusCode)
+				t.Logf("POST %v [%v]: ...", baseURL+"/todo/abc", resp.StatusCode)
 
-		// 		// Validate the GET /todo/{id} endpoint
-		// 		resp, err = http.Get(baseURL + "/todo/abc")
-		// 		assert.NoError(t, err, "expected to be able to GET /todo/{id}")
-		// 		bytes, err = ioutil.ReadAll(resp.Body)
-		// 		assert.NoError(t, err)
-		// 		assert.Equal(t, 401, resp.StatusCode)
-		// 		assert.Equal(t, "Authorization header required", string(bytes))
-		// 		t.Logf("GET %v [%v]: %v", baseURL+"/todo/abc", resp.StatusCode, string(bytes))
+				// Validate the GET /todo/{id} endpoint
+				resp, err = http.Get(baseURL + "/todo/abc")
+				assert.NoError(t, err, "expected to be able to GET /todo/{id}")
+				bytes, err = ioutil.ReadAll(resp.Body)
+				assert.NoError(t, err)
+				assert.Equal(t, 401, resp.StatusCode)
+				assert.Equal(t, "Authorization header required", string(bytes))
+				t.Logf("GET %v [%v]: %v", baseURL+"/todo/abc", resp.StatusCode, string(bytes))
 
-		// 		// Validate the GET /todo endpoint
-		// 		resp, err = http.Get(baseURL + "/todo/")
-		// 		assert.NoError(t, err, "expected to be able to GET /todo")
-		// 		assert.Equal(t, int64(28), resp.ContentLength)
-		// 		bytes, err = ioutil.ReadAll(resp.Body)
-		// 		assert.NoError(t, err)
-		// 		t.Logf("GET %v [%v]: %v", baseURL+"/todo", resp.StatusCode, string(bytes))
-		// 	},
-		// },
-		// {
-		// 	Dir: path.Join(cwd, "../../examples/timers"),
-		// 	Config: map[string]string{
-		// 		"aws:config:region":     region,
-		// 		"cloud:config:provider": "aws",
-		// 		"timers:config:message": "Hello, Pulumi Timers!",
-		// 	},
-		// 	Dependencies: []string{
-		// 		"pulumi",
-		// 		"@pulumi/cloud",
-		// 		"@pulumi/cloud-aws",
-		// 	},
-		// },
+				// Validate the GET /todo endpoint
+				resp, err = http.Get(baseURL + "/todo/")
+				assert.NoError(t, err, "expected to be able to GET /todo")
+				assert.Equal(t, int64(28), resp.ContentLength)
+				bytes, err = ioutil.ReadAll(resp.Body)
+				assert.NoError(t, err)
+				t.Logf("GET %v [%v]: %v", baseURL+"/todo", resp.StatusCode, string(bytes))
+			},
+		},
+		{
+			Dir: path.Join(cwd, "../../examples/timers"),
+			Config: map[string]string{
+				"aws:config:region":     region,
+				"cloud:config:provider": "aws",
+				"timers:config:message": "Hello, Pulumi Timers!",
+			},
+			Dependencies: []string{
+				"pulumi",
+				"@pulumi/cloud",
+				"@pulumi/cloud-aws",
+			},
+		},
 		{
 			Dir: path.Join(cwd, "../../examples/httpEndpoint"),
 
