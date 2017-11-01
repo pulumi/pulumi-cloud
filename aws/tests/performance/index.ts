b69bb1834a12bc5f2ffbe8b189d72b16f11637c1
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-11-01 12:10:38 -0700

    Record more data.

--- aws/tests/performance/index.ts
+++ aws/tests/performance/index.ts
@@ -1,26 +1,14 @@
 // Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
 
 import * as cloud from "@pulumi/cloud";
-import * as metricTypes from "datadog-metrics";
-import * as debugTypes from "debug";
-import * as util from "util";
+import * as metrics from "datadog-metrics";
 
 const endpoint = new cloud.HttpEndpoint("performance");
 const table = new cloud.Table("table");
+let currentTableId = 0;
 
 endpoint.get("/performance", async (req, res) => {
     try {
-        process.env.DEBUG = "metrics";
-        const debug: typeof debugTypes = require("debug");
-
-        let debugLog = "Log never called";
-        (<any>debug).log = function () {
-            const formatted = util.format.apply(util, eval("arguments")) + "\n";
-            debugLog += formatted;
-        };
-
-        const metrics: typeof metricTypes = require("datadog-metrics");
-
         res.setHeader("Content-Type", "text/html");
         metrics.init({
             apiKey: "ff87b4efa3b161051bb2fa8d879e2597",
@@ -28,15 +16,8 @@ endpoint.get("/performance", async (req, res) => {
             prefix: "perf-tests-",
         });
 
-        const logger = new metrics.BufferedMetricsLogger({
-            apiKey: "ff87b4efa3b161051bb2fa8d879e2597",
-            appKey: "331ffc1f561a0732d16112a069b9d59f65e406f6",
-            prefix: "perf-tests-",
-            flushIntervalSeconds: 15
-        });
-
-        const totalTime = await recordAndReportTime("all", metrics, async () => {
-            await testTablePerformance(metrics);
+        const totalTime = await recordAndReportTime("all", async () => {
+            await testTablePerformance();
         });
 
         // Ensure all our perf metrics are uploaded.
@@ -44,7 +25,7 @@ endpoint.get("/performance", async (req, res) => {
             metrics.flush(resolve, reject);
         });
 
-        res.end("Perf test completed successfully: " + totalTime + ", " + debugLog);
+        res.end("Perf test completed successfully: " + totalTime);
     } catch (err) {
         res.end("Perf test failed: " + err + "\n" + err.stack);
     }
@@ -53,7 +34,7 @@ endpoint.get("/performance", async (req, res) => {
 const deployment = endpoint.publish()
 deployment.url.then(u => console.log("Serving at: " + u));
 
-async function recordAndReportTime(name: string, metrics: typeof metricTypes, code: () => Promise<void>) {
+async function recordAndReportTime(name: string, code: () => Promise<void>) {
     const start = process.hrtime();
 
     await code();
@@ -66,19 +47,87 @@ async function recordAndReportTime(name: string, metrics: typeof metricTypes, co
     return ms;
 }
 
-async function testTablePerformance(metrics: typeof metricTypes) {
-    await recordAndReportTime("table-all", metrics, async() => {
-        await testTableInsertPerformance(metrics);
+async function testTablePerformance() {
+    // warm things up first.
+    {
+        const promises: any[] = [];
+        for (let i = 0; i < 10; i++, currentTableId++) {
+            promises.push(table.insert({id: currentTableId.toString(), value: currentTableId}));
+        }
+
+        await Promise.all(promises);
+    }
+
+    await recordAndReportTime("table-all", async() => {
+        await testTableInsertPerformance();
+        await testTableGetPerformance();
+        await testTableScanPerformance();
     });
 }
 
-async function testTableInsertPerformance(metrics: typeof metricTypes) {
-    await recordAndReportTime("table-insert", metrics, async() => {
-        const promises: any[] = [];
+async function testTableScanPerformance() {
+    await recordAndReportTime("table-scan-sequential", async() => {
         for (let i = 0; i < 10; i++) {
-            promises.push(table.insert({id: i.toString(), value: i}));
+            await table.scan();
+        }
+    });
+
+    await recordAndReportTime("table-scan-parallel", async() => {
+        const promises: any[] = [];
+        for (let i = 0; i < 10; i++, currentTableId++) {
+            promises.push(table.scan());
         }
 
         await Promise.all(promises);
     });
 }
+
+async function testTableInsertPerformance() {
+    await recordAndReportTime("table-insert-sequential", async() => {
+        for (let i = 0; i < 10; i++, currentTableId++) {
+            await table.insert({id: currentTableId.toString(), value: currentTableId});
+        }
+    });
+
+    await recordAndReportTime("table-insert-parallel", async() => {
+        const promises: any[] = [];
+        for (let i = 0; i < 10; i++, currentTableId++) {
+            promises.push(table.insert({id: currentTableId.toString(), value: currentTableId}));
+        }
+
+        await Promise.all(promises);
+    });
+}
+
+async function testTableGetPerformance() {
+    await recordAndReportTime("table-get-sequential-existing", async() => {
+        for (let i = 0; i < 10; i++) {
+            await table.get({id: i.toString()});
+        }
+    });
+
+    await recordAndReportTime("table-get-parallel-existing", async() => {
+        const promises: any[] = [];
+        for (let i = 0; i < 10; i++) {
+            promises.push(table.get({id: i.toString()}));
+        }
+
+        await Promise.all(promises);
+    });
+
+    await recordAndReportTime("table-get-sequential-missing", async() => {
+        for (let i = 0; i < 10; i++) {
+            await table.get({id: "-1"});
+        }
+    });
+
+    await recordAndReportTime("table-get-parallel-missing", async() => {
+        const promises: any[] = [];
+        for (let i = 0; i < 10; i++) {
+            promises.push(table.get({id: "-1"}));
+        }
+
+        await Promise.all(promises);
+    });
+}
+
