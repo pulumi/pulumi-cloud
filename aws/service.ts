cb8f310372b9d2cc578a30b308cdbb752f9f155f
Luke Hoban <luke@pulumi.com>
2017-10-31 22:20:58 -0700

    Add some additional diagnostics

--- aws/service.ts
+++ aws/service.ts
@@ -337,9 +337,14 @@ async function buildAndPushImage(imageName: string, container: cloud.Container,
     }
 
     // Finally, inspect the image so we can return the SHA digest.
-    const inspectResult = await runCLICommand("docker", ["inspect", "-f", "{{.Id}}", imageName], buildPath, true);
+    const inspectResult = await runCLICommand(
+        "docker",
+        ["image", "inspect", "-f", "{{.Id}}", imageName],
+        buildPath,
+        true,
+    );
     if (inspectResult.code || !inspectResult.stdout) {
-        throw new Error(`No digest available for image ${imageName}`);
+        throw new Error(`No digest available for image ${imageName}: ${inspectResult.code} -- ${inspectResult.stdout}`);
     }
     return inspectResult.stdout.trim();
 }
