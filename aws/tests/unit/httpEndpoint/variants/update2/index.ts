87582447988253d3e549f89747b30cbe49cb1dec
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-11-07 14:34:03 -0800

    Add initial httpendpoint stubs

---
+++ aws/tests/unit/httpEndpoint/variants/update2/index.ts
@@ -0,0 +1,75 @@
+// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+
+import * as cloud from "@pulumi/cloud";
+import * as assert from "assert";
+
+namespace updateProgramTests {
+    const table1 = new cloud.Table("persistent_table");
+    export async function testPersistentTable() {
+        // in v2 of the program make sure only half the data is still there.
+        for (let i = 0; i < 10; i++) {
+            const result = await table1.get({[table1.primaryKey]: "" + i });
+            if (i % 2 === 0) {
+                assert.equal(undefined, result);
+            }
+            else {
+                assert.equal(result.value1, i);
+            }
+        }
+    }
+}
+
+const endpoint = new cloud.HttpEndpoint("unittests");
+
+endpoint.get("/unittests", async (req, res) => {
+    try {
+        const [passed, json] = await runAllTests();
+        if (passed) {
+            res.json(json);
+        }
+        else {
+            res.status(500).json(json);
+        }
+    } catch (err) {
+        res.status(500).json(errorJSON(err));
+    }
+});
+
+const deployment = endpoint.publish();
+deployment.url.then(u => console.log("Serving at: " + u));
+
+function errorJSON(err: any) {
+    const result: any = Object.create(null);
+    Object.getOwnPropertyNames(err).forEach(key => result[key] = err[key]);
+    return result;
+}
+
+async function runAllTests(): Promise<[boolean, any]> {
+    let passed = true;
+    const result: any = Object.create(null);
+
+    passed = await runTests("tableTests.updateProgramTests", updateProgramTests, result) && passed;
+
+    return [passed, result];
+}
+
+async function runTests(moduleName: string, module: any, result: any) {
+    let passed = true;
+    for (const name of Object.keys(module)) {
+        if (!name.startsWith("test")) {
+            continue;
+        }
+
+        const fullName = `${moduleName}.${name}`;
+        try {
+            await module[name]();
+            result[fullName] = "passed";
+        }
+        catch (err) {
+            passed = false;
+            result[fullName] = errorJSON(err);
+        }
+    }
+
+    return passed;
+}
---
+++ aws/tests/unit/unit_test.go
@@ -0,0 +1,129 @@
+package examples
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/tokens"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pulumi/pulumi-cloud/pkg/pulumiframework"
+	"github.com/pulumi/pulumi/pkg/resource"
+	"github.com/pulumi/pulumi/pkg/resource/stack"
+	"github.com/pulumi/pulumi/pkg/testing/integration"
+)
+
+func Test_Performance(t *testing.T) {
+	region := os.Getenv("AWS_REGION")
+	if region == "" {
+		t.Skipf("Skipping test due to missing AWS_REGION environment variable")
+	}
+	fmt.Printf("AWS Region: %v\n", region)
+
+	cwd, err := os.Getwd()
+	if !assert.NoError(t, err, "expected a valid working directory: %v", err) {
+		return
+	}
+	tests := []integration.ProgramTestOptions{
+		{
+			Dir: cwd + "/table",
+			Config: map[string]string{
+				"aws:config:region":     region,
+				"cloud:config:provider": "aws",
+			},
+			Dependencies: []string{
+				"@pulumi/cloud",
+				"@pulumi/cloud-aws",
+			},
+			ExtraRuntimeValidation: func(t *testing.T, checkpoint stack.Checkpoint) {
+				hitJSONEndpoint(t, checkpoint, "unittests", "unittests", "unittests")
+			},
+			EditDirs: []integration.EditDir{
+				{
+					Dir: cwd + "/table/variants/update1",
+					ExtraRuntimeValidation: func(t *testing.T, checkpoint stack.Checkpoint) {
+						hitJSONEndpoint(t, checkpoint, "unittests", "unittests", "/unittests")
+					},
+				},
+				{
+					Dir: cwd + "/table/variants/update2",
+					ExtraRuntimeValidation: func(t *testing.T, checkpoint stack.Checkpoint) {
+						hitJSONEndpoint(t, checkpoint, "unittests", "unittests", "/unittests")
+					},
+				},
+			},
+		},
+		{
+			Dir: cwd + "/httpEndpoint",
+			Config: map[string]string{
+				"aws:config:region":     region,
+				"cloud:config:provider": "aws",
+			},
+			Dependencies: []string{
+				"@pulumi/cloud",
+				"@pulumi/cloud-aws",
+			},
+			ExtraRuntimeValidation: func(t *testing.T, checkpoint stack.Checkpoint) {
+				hitJSONEndpoint(t, checkpoint, "unittests", "unittests", "unittests")
+			},
+			EditDirs: []integration.EditDir{
+				{
+					Dir: cwd + "/httpEndpoint/variants/update1",
+					ExtraRuntimeValidation: func(t *testing.T, checkpoint stack.Checkpoint) {
+						hitJSONEndpoint(t, checkpoint, "unittests", "unittests", "/unittests")
+					},
+				},
+				{
+					Dir: cwd + "/httpEndpoint/variants/update2",
+					ExtraRuntimeValidation: func(t *testing.T, checkpoint stack.Checkpoint) {
+						hitJSONEndpoint(t, checkpoint, "unittests", "unittests", "/unittests")
+					},
+				},
+			},
+		},
+	}
+	for _, ex := range tests {
+		test := ex
+		t.Run(test.Dir, func(t *testing.T) {
+			integration.ProgramTest(t, test)
+		})
+	}
+}
+
+func hitJSONEndpoint(
+	t *testing.T,
+	checkpoint stack.Checkpoint,
+	packageName tokens.PackageName,
+	endpointName tokens.QName,
+	urlPortion string) {
+
+	_, _, snapshot, err := stack.DeserializeCheckpoint(&checkpoint)
+	if !assert.Nil(t, err, "expected checkpoint deserialization to succeed") {
+		return
+	}
+	pulumiResources := pulumiframework.GetComponents(snapshot.Resources)
+	urn := resource.NewURN(checkpoint.Target, packageName, "pulumi:framework:Endpoint", endpointName)
+	endpoint := pulumiResources[urn]
+	if !assert.NotNil(t, endpoint, "expected to find endpoint") {
+		return
+	}
+	baseURL := endpoint.Properties["url"].StringValue()
+	assert.NotEmpty(t, baseURL, fmt.Sprintf("expected a `%v` endpoint", endpointName))
+
+	// Validate the GET /unittests endpoint
+
+	resp, err := http.Get(baseURL + urlPortion)
+	assert.NoError(t, err, "expected to be able to GET "+baseURL+urlPortion)
+
+	contentType := resp.Header.Get("Content-Type")
+	assert.Equal(t, "application/json", contentType)
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+	t.Logf("GET %v [%v/%v]: %v", baseURL+urlPortion, resp.StatusCode, contentType, string(bytes))
+}
