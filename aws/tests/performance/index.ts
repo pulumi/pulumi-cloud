e3168f817730c104379597f481232c3d6a39c4f0
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-11-06 16:52:56 -0800

    Working perf runner

--- aws/tests/performance/index.ts
+++ aws/tests/performance/index.ts
@@ -7,14 +7,14 @@ interface TestInfo { name: string; apiKey: string; appKey: string; }
 interface TestResults { [name: string]: [/*passed*/ boolean, /*testTime:*/ number]; }
 
 const testResultTable = new cloud.Table("test-results");
-const testResultKey = { id: 0 };
+const testResultKey = { id: "0" };
 
 const topic = new cloud.Topic<TestInfo>("topic");
 const table = new cloud.Table("table");
 
 const tests: {[name: string]: [(record: boolean) => Promise<number>, number]} = {
-    table: [testTablePerformance, /*repeat*/ 20],
-    httpEndpoint: [testHttpEndpointPerformance, /*repeat*/ 2],
+    tableTests: [testTablePerformance, /*repeat*/ 20],
+    httpEndpointTests: [testHttpEndpointPerformance, /*repeat*/ 2],
 };
 
 topic.subscribe("test-performance", async(info: TestInfo) => {
@@ -44,49 +44,6 @@ topic.subscribe("test-performance", async(info: TestInfo) => {
     testResultTable.update(testResultKey, { [testName]: [true, totalTime] });
 });
 
-const endpoint = new cloud.HttpEndpoint("performance");
-
-endpoint.get("/start-performance-tests", async (req, res) => {
-    // Initialize all test results.
-    await testResultTable.insert(testResultKey);
-
-    for (const testName of Object.keys(tests)) {
-        await testResultTable.update(testResultKey, { [testName]: [false, 0] });
-    }
-
-    const apiKey = <string>req.query.DATADOG_API_KEY;
-    const appKey = <string>req.query.DATADOG_APP_KEY;
-
-    await Promise.all(
-        Object.keys(tests).map(name => topic.publish({ name, apiKey, appKey })));
-});
-
-endpoint.get("/check-performance-tests", async (req, res) => {
-    const testResults: TestResults = await testResultTable.get(testResultKey);
-    const result = Object.create(null);
-
-    for (const key of Object.keys(testResults)) {
-        if (key !== testResultTable.primaryKey) {
-            const singleResult = testResults[key];
-            if (!singleResult[0]) {
-                result.status = "running";
-            }
-            else {
-                result[key] = singleResult[1];
-            }
-        }
-    }
-
-    if (!result.status) {
-        result.status = "complete";
-    }
-
-    res.json(result);
-});
-
-const deployment = endpoint.publish();
-deployment.url.then(u => console.log("Serving at: " + u));
-
 async function recordAndReportTime(record: boolean, name: string, code: () => Promise<void>) {
     const start = process.hrtime();
 
@@ -102,12 +59,6 @@ async function recordAndReportTime(record: boolean, name: string, code: () => Pr
 
     return ms;
 }
-
-// async function testAll(record: boolean) {
-//     return await recordAndReportTime(record, "all", async () => {
-//         await testTablePerformance(record);
-//     });
-// }
 
 async function testHttpEndpointPerformance(record: boolean) {
     return await recordAndReportTime(record, "httpEndpoint-all", () => Promise.resolve());
@@ -143,3 +94,65 @@ async function testTableGetPerformance(record: boolean) {
     });
 }
 
+
+const endpoint = new cloud.HttpEndpoint("performance");
+
+endpoint.get("/start-performance-tests", async (req, res) => {
+    try {
+        // Initialize all test results.
+        await testResultTable.insert(testResultKey);
+
+        for (const testName of Object.keys(tests)) {
+            await testResultTable.update(testResultKey, { [testName]: [false, 0] });
+        }
+
+        const apiKey = <string>req.query.DATADOG_API_KEY;
+        const appKey = <string>req.query.DATADOG_APP_KEY;
+
+        await Promise.all(
+            Object.keys(tests).map(name => topic.publish({ name, apiKey, appKey })));
+
+        res.setHeader("Content-Type", "text/html");
+        res.end("Performance tests started.");
+    }
+    catch (err) {
+        res.status(500).json(errorJSON(err));
+    }
+});
+
+endpoint.get("/check-performance-tests", async (req, res) => {
+    try {
+        const testResults: TestResults = await testResultTable.get(testResultKey);
+        const result = Object.create(null);
+
+        for (const testName of Object.keys(tests)) {
+            if (!testResults || !testResults[testName]) {
+                result.status = result[testName] = "not started";
+            }
+            else if (!testResults[testName][0]) {
+                result.status = result[testName] = "running";
+            }
+            else {
+                result[testName] = testResults[testName][1];
+            }
+        }
+
+        if (!result.status) {
+            result.status = "complete";
+        }
+
+        res.json(result);
+    }
+    catch (err) {
+        res.status(500).json(errorJSON(err));
+    }
+});
+
+function errorJSON(err: any) {
+    const result: any = Object.create(null);
+    Object.getOwnPropertyNames(err).forEach(key => result[key] = err[key]);
+    return result;
+}
+
+const deployment = endpoint.publish();
+deployment.url.then(u => console.log("Serving at: " + u));
