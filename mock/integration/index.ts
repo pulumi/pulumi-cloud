66eca61272fa3a23f29dbe128ccdb99eb614ad7c
Matt Ellis <matt@pulumi.com>
2017-11-12 21:03:30 -0800

    Adopt new makefile system
    See https://github.com/pulumi/home/pull/56 for details.

--- mock/integration/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "pulumi";
-pulumi.runtime.setConfig("cloud:config:provider", "mock");
-
-import * as examples from "@pulumi/integration-examples";
-examples.exampleTwitter2();
