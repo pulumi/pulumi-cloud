230c572e7e361bdce140827ac3c63faca4baedb0
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-11-14 13:36:26 -0800

    Name things clearly to ensure we know what resources belong to tests.

--- aws/tests/unit/variants/update1/httpEndpointTests.ts
+++ aws/tests/unit/variants/update1/httpEndpointTests.ts
@@ -5,7 +5,7 @@ import * as assert from "assert";
 import * as supertest from "supertest";
 import * as harness from "./harness";
 
-const endpoint = new cloud.HttpEndpoint("unittests_endpoint");
+const endpoint = new cloud.HttpEndpoint("tests-endpoint");
 
 namespace updateProgramTests {
     endpoint.get("/persistent1", async (req, res) => {
