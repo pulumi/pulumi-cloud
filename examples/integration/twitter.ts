f767dbbac76434507ec8e4feba1122497b72a280
Matt Ellis <matt@pulumi.com>
2017-11-16 22:39:20 -0800

    Fix lint issues in examples

--- examples/integration/twitter.ts
+++ examples/integration/twitter.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "pulumi";
 import * as cloud from "@pulumi/cloud";
-import * as utils from "./utils"
+import * as pulumi from "pulumi";
 import { poll } from "./poll";
+import * as utils from "./utils";
 
 // Consumer key and secret from https://apps.twitter.com/.  Create a new app and request a
 // request these to make API requests on behalf of the logged in account.
@@ -19,20 +19,20 @@ async function getTwitterAuthorizationBearer(): Promise<string> {
 
     if (cachedToken === undefined) {
         console.log("Bearer token not in cache. Retrieving from twitter...");
-        let credentials = new Buffer(keyAndSecret).toString('base64');
+        let credentials = new Buffer(keyAndSecret).toString("base64");
 
-        let url = 'https://api.twitter.com/oauth2/token';
+        let url = "https://api.twitter.com/oauth2/token";
 
         let request = require("request-promise-native");
         let body = await request({
             url: url,
-            method:'POST',
+            method: "POST",
             headers: {
                 "Authorization": "Basic " + credentials,
-                "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8"
+                "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
             },
             body: "grant_type=client_credentials",
-            json: true
+            json: true,
         });
 
         let accessToken = body.access_token;
@@ -50,7 +50,7 @@ export function search(name: string, term: string): cloud.Stream<Tweet> {
     console.log("Creating poll...");
     let searchPoll = poll<Tweet>(name, {minutes: 1}, async (lastToken) => {
         console.log("Getting bearer token...");
-        var bearerToken = await getTwitterAuthorizationBearer();
+        let bearerToken = await getTwitterAuthorizationBearer();
 
         console.log("Running poll...");
         let request = require("request-promise-native");
