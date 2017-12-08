33fd1c8177bcbd5853140d0a4fcecae271c8f323
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-12-08 15:24:29 -0800

    Tests that match aaf283793aaeb8c7e1b92964359ad8ab19539c60

--- aws/aws_test.go
+++ aws/aws_test.go
@@ -362,7 +362,7 @@ func Test_Examples(t *testing.T) {
 			ReportStats: integration.NewS3Reporter("us-west-2", "eng.pulumi.com", "testreports"),
 		})
 		t.Run(example.Dir, func(t *testing.T) {
-			integration.TestLifeCycle(t, &example)
+			integration.ProgramTest(t, &example)
 		})
 	}
 }
