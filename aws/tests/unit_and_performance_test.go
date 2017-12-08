5745f9923b65c877311588b17f1688f2dd609231
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-12-07 16:45:43 -0800

    Test fallout.

--- aws/tests/unit_and_performance_test.go
+++ aws/tests/unit_and_performance_test.go
@@ -139,7 +139,7 @@ func Test_Performance(t *testing.T) {
 	for _, ex := range tests {
 		test := ex
 		t.Run(test.Dir, func(t *testing.T) {
-			integration.ProgramTest(t, test)
+			integration.TestLifeCycle(t, &test)
 		})
 	}
 }
