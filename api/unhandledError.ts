e18a76f4c7697849b697ef3588a9afe046722648
Matthew Riley <mattdr@pulumi.com>
2018-01-30 00:41:04 -0800

    Use more familiar function type declaration

--- api/unhandledError.ts
+++ api/unhandledError.ts
@@ -13,4 +13,4 @@ export type ErrorHandler = (message: string, payload: any) => void;
  * @param name The name of this gobal error handler.
  * @param handler The callback to invoke on unhandled errors.
  */
-export let onError: { (name: string, handler: ErrorHandler): void };
+export let onError: (name: string, handler: ErrorHandler) => void;
