1e78b7d9f186e1b92bef7dc03a57396853a499a4
Cyrus Najmabadi <cyrus@pulumi.com>
2018-03-10 14:03:27 -0800

    Update example.

--- examples/crawler/support/index.ts
+++ examples/crawler/support/index.ts
@@ -1,13 +1,15 @@
-import { parse, resolve } from "url";
+import * as urlModule from "url";
 
 // Canonicalize URLs - remove authorization, fragment and search parts.
 export function canonicalUrl(url: string, baseUrl: string): string {
-    let relUrl = resolve(baseUrl, url);
-    let parts = parse(relUrl);
+    const urlMod: typeof urlModule = require("url");
+    let relUrl = urlMod.resolve(baseUrl, url);
+    let parts = urlMod.parse(relUrl);
     return parts.protocol + "//" + parts.host + parts.pathname;
 }
 
 // Extract the hostname part from a URL
 export function hostname(url: string): string | undefined {
-    return parse(url).hostname;
+    const urlMod: typeof urlModule = require("url");
+    return urlMod.parse(url).hostname;
 }
