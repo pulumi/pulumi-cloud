17d006ab22eca6bd55b4fdf4d0ae2c625dec2154
Luke Hoban <luke@pulumi.com>
2018-03-12 17:10:25 -0700

    Remove the mock implementation

--- pkg/unused.go
+++
@@ -1,5 +0,0 @@
-package pkg
-
-// Unused is not used.  It is only here to ensure that we have compilable Go code in this repo so that it can be
-// referenced in `dep` dependencies from other repos.
-const Unused = "unused"
