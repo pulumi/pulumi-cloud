81042aa595c3f521e0a4b843033c3fd0bb679753
Cyrus Najmabadi <cyrus@pulumi.com>
2018-05-02 12:53:42 -0700

    Add the initial files in.

---
+++ examples/jsExample/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const pulumi = require("@pulumi/pulumi");
+const cloud = require("@pulumi/cloud");
+
+// Create an API endpoint
+let endpoint = new cloud.HttpEndpoint("hello-world");
+
+endpoint.get("/foo", async (req, res) => {
+    res.json({ success: true });
+    res.status(200);
+});
+
+module.exports.endpoint = endpoint.publish().url;
