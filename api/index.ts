f55fc73f96c13a92bac769bc1fe23883129966fd
Luke Hoban <lukehoban@gmail.com>
2018-05-23 13:38:02 -0700

    Relicense under Apache 2.0 (#487)

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,16 @@
-// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // This is the core location where the API package actually exports code for dependent packages to
 // use.  We are effectively reflecting over the user's current environment and using that to switch
