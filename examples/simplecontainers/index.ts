89237c5ed69f6e7dbd6a8ab63634fd3a29e90b52
Pat Gavlin <pat@pulumi.com>
2018-05-31 17:47:18 -0700

    Move tests around

--- examples/simplecontainers/index.ts
+++ examples/simplecontainers/index.ts
@@ -15,34 +15,4 @@ let nginx = new cloud.Service("examples-nginx2", {
     replicas: 2,
 });
 
-let cachedNginx = new cloud.Service("examples-cached-nginx", {
-    containers: {
-        nginx: {
-            build: {
-                context: "./app",
-                cacheFrom: true,
-            },
-            memory: 128,
-            ports: [{port: 80, protocol: "http" }],
-        },
-    },
-    replicas: 2,
-});
-
-let multistageCachedNginx = new cloud.Service("examples-multistage-cached-nginx", {
-    containers: {
-        nginx: {
-            build: {
-                context: "./app",
-                dockerfile: "./app/Dockerfile-multistage",
-                cacheFrom: {stages: ["build"]},
-            },
-            memory: 128,
-            ports: [{port: 80, protocol: "http" }],
-        },
-    },
-    replicas: 2,
-});
-
-
 export let nginxEndpoint: Output<string> = nginx.defaultEndpoint.apply(ep => `http://${ep.hostname}:${ep.port}`);
