21de120e76251a93e1af89118329d3c25813dade
Cyrus Najmabadi <cyrus@pulumi.com>
2018-08-31 16:56:34 -0700

    Add express test.

--- azure/azure_test.go
+++ azure/azure_test.go
@@ -91,6 +91,14 @@ func Test_Examples(t *testing.T) {
 				"@pulumi/cloud-azure",
 			},
 		},
+		{
+			Dir:    path.Join(cwd, "./examples/express"),
+			Config: commonConfig,
+			Dependencies: []string{
+				"@pulumi/cloud",
+				"@pulumi/cloud-azure",
+			},
+		},
 	}
 
 	longExamples := []integration.ProgramTestOptions{}
