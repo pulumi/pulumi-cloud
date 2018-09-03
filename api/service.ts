dacbeb46774c14b5fe55293e650619b5d34fbf4a
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2018-09-03 14:12:02 -0700

    Get cloud.Service working on Azure. (#564)

--- api/service.ts
+++ api/service.ts
@@ -71,6 +71,8 @@ export interface Container {
      * The maximum amount of memory the container will be allowed to use. Maps to the Docker
      * `--memory` option - see
      * https://docs.docker.com/engine/reference/commandline/run.
+     *
+     * This should be supplied in MB. i.e. A value of 1024 would equal one gigabyte.
      */
     memory?: pulumi.Input<number>;
     /**
@@ -79,6 +81,8 @@ export interface Container {
      * `memory` and `memoryReservation` must be specified.  Maps to the Docker
      * `--memory-reservation` option - see
      * https://docs.docker.com/engine/reference/commandline/run.
+     *
+     * This should be supplied in MB. i.e. A value of 1024 would equal one gigabyte.
      */
     memoryReservation?: pulumi.Input<number>;
     /**
