c700dd3e9c684bfb650a2dfed74d649fa2877213
Cyrus Najmabadi <cyrus@pulumi.com>
2018-09-03 18:02:58 -0700

    Initial azure topic work

--- azure/topic.ts
+++ azure/topic.ts
@@ -15,20 +15,40 @@
 import * as azure from "@pulumi/azure";
 import * as cloud from "@pulumi/cloud";
 import * as pulumi from "@pulumi/pulumi";
+import * as shared from "./shared";
 
 export class Topic<T> extends pulumi.ComponentResource implements cloud.Topic<T> {
+    public readonly namespace: azure.eventhub.Namespace;
+    public readonly topic: azure.eventhub.Topic;
+
     public readonly publish: (item: T) => Promise<void>;
 
     // Outside API (constructor and methods)
+    public readonly subscribe: (name: string, handler: (item: T) => Promise<void>) => void;
 
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super("cloud:topic:Topic", name, {}, opts);
 
-        this.publish = _ => { throw new Error("Method not implemented."); };
-        throw new Error("Method not implemented.");
-    }
+        const ns = new azure.eventhub.Namespace(name, {
+            location: shared.location,
+            resourceGroupName: shared.globalResourceGroupName,
+            // topics are only supported in standard and premium.
+            sku: "standard",
+        }, { parent: this });
 
-    public subscribe(name: string, handler: (item: T) => Promise<void>) {
-        throw new Error("Method not implemented.");
+        const topic = new azure.eventhub.Topic(name, {
+            location: shared.location,
+            resourceGroupName: shared.globalResourceGroupName,
+            namespaceName: ns.name,
+        });
+
+        this.namespace = ns;
+        this.topic = topic;
+
+        this.publish = _ => { throw new Error("Method not implemented."); };
+
+        this.subscribe = (name, handler) => {
+            const subscription =
+        };
     }
 }
