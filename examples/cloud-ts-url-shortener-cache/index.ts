132af540b6dd5d9c707b04617029cbf294debd81
Cyrus Najmabadi <cyrus@pulumi.com>
2018-09-06 22:42:14 -0700

    Eanable things

--- examples/cloud-ts-url-shortener-cache/index.ts
+++ examples/cloud-ts-url-shortener-cache/index.ts
@@ -28,69 +28,69 @@ let urlCache = new cache.Cache("urlcache");
 let endpoint = new cloud.HttpServer("my", () => {
     let app = express();
 
-    // // GET /url lists all URLs currently registered.
-    // app.get("/url", asyncMiddleware(async (req, res) => {
-    //     try {
-    //         let items = await urlTable.scan();
-    //         res.status(200).json(items);
-    //         console.log(`GET /url retrieved ${items.length} items`);
-    //     } catch (err) {
-    //         res.status(500).json(err.stack);
-    //         console.log(`GET /url error: ${err.stack}`);
-    //     }
-    // }));
+    // GET /url lists all URLs currently registered.
+    app.get("/url", asyncMiddleware(async (req, res) => {
+        try {
+            let items = await urlTable.scan();
+            res.status(200).json(items);
+            console.log(`GET /url retrieved ${items.length} items`);
+        } catch (err) {
+            res.status(500).json(err.stack);
+            console.log(`GET /url error: ${err.stack}`);
+        }
+    }));
 
-    // // GET /url/{name} redirects to the target URL based on a short-name.
-    // app.get("/url/:name", asyncMiddleware(async (req, res) => {
-    //     let name = req.params.name
-    //     try {
-    //         // First try the Redis cache.
-    //         let url = await urlCache.get(name);
-    //         if (url) {
-    //             console.log(`Retrieved value from Redis: ${url}`);
-    //             res.setHeader("X-Powered-By", "redis");
-    //         }
-    //         else {
-    //             // If we didn't find it in the cache, consult the table.
-    //             let value = await urlTable.get({name});
-    //             url = value && value.url;
-    //             if (url) {
-    //                 urlCache.set(name, url); // cache it for next time.
-    //             }
-    //         }
+    // GET /url/{name} redirects to the target URL based on a short-name.
+    app.get("/url/:name", asyncMiddleware(async (req, res) => {
+        let name = req.params.name
+        try {
+            // First try the Redis cache.
+            let url = await urlCache.get(name);
+            if (url) {
+                console.log(`Retrieved value from Redis: ${url}`);
+                res.setHeader("X-Powered-By", "redis");
+            }
+            else {
+                // If we didn't find it in the cache, consult the table.
+                let value = await urlTable.get({name});
+                url = value && value.url;
+                if (url) {
+                    urlCache.set(name, url); // cache it for next time.
+                }
+            }
 
-    //         // If we found an entry, 301 redirect to it; else, 404.
-    //         if (url) {
-    //             res.setHeader("Location", url);
-    //             res.status(302);
-    //             res.end("");
-    //             console.log(`GET /url/${name} => ${url}`)
-    //         }
-    //         else {
-    //             res.status(404);
-    //             res.end("");
-    //             console.log(`GET /url/${name} is missing (404)`)
-    //         }
-    //     } catch (err) {
-    //         res.status(500).json(err.stack);
-    //         console.log(`GET /url/${name} error: ${err.stack}`);
-    //     }
-    // }));
+            // If we found an entry, 301 redirect to it; else, 404.
+            if (url) {
+                res.setHeader("Location", url);
+                res.status(302);
+                res.end("");
+                console.log(`GET /url/${name} => ${url}`)
+            }
+            else {
+                res.status(404);
+                res.end("");
+                console.log(`GET /url/${name} is missing (404)`)
+            }
+        } catch (err) {
+            res.status(500).json(err.stack);
+            console.log(`GET /url/${name} error: ${err.stack}`);
+        }
+    }));
 
-    // // POST /url registers a new URL with a given short-name.
-    // app.post("/url", asyncMiddleware(async (req, res) => {
-    //     let url = <string>req.query["url"];
-    //     let name = <string>req.query["name"];
-    //     try {
-    //         await urlTable.insert({ name, url });
-    //         await urlCache.set(name, url);
-    //         res.json({ shortenedURLName: name });
-    //         console.log(`POST /url/${name} => ${url}`);
-    //     } catch (err) {
-    //         res.status(500).json(err.stack);
-    //         console.log(`POST /url/${name} => ${url} error: ${err.stack}`);
-    //     }
-    // }));
+    // POST /url registers a new URL with a given short-name.
+    app.post("/url", asyncMiddleware(async (req, res) => {
+        let url = <string>req.query["url"];
+        let name = <string>req.query["name"];
+        try {
+            await urlTable.insert({ name, url });
+            await urlCache.set(name, url);
+            res.json({ shortenedURLName: name });
+            console.log(`POST /url/${name} => ${url}`);
+        } catch (err) {
+            res.status(500).json(err.stack);
+            console.log(`POST /url/${name} => ${url} error: ${err.stack}`);
+        }
+    }));
 
     // Serve all files in the www directory to the root.
     // Note: www will be auto-included using config. either
