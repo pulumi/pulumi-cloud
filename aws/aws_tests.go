87769a8dca70b5d1f9049c015e510dd702a9b507
Cyrus Najmabadi <cyrusn@pulumi.com>
2019-01-07 14:45:32 -0800

    Fix tests.

--- aws/aws_tests.go
+++ aws/aws_tests.go
@@ -50,7 +50,7 @@ func RunAwsTests(t *testing.T) {
 	fmt.Printf("AWS Region: %v\n", region)
 
 	cwd, err := os.Getwd()
-	cwd = path.Join(cwd, "azure")
+	cwd = path.Join(cwd, "aws")
 	if !assert.NoError(t, err, "expected a valid working directory: %v", err) {
 		return
 	}
