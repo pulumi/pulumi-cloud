5e27033364f1b8f1556ee24555c16d1f855d2a3b
Cyrus Najmabadi <cyrus@pulumi.com>
2019-01-16 10:01:36 -0800

    REvert

--- aws/aws_tests.go
+++ aws/aws_tests.go
@@ -55,11 +55,11 @@ func RunAwsTests(t *testing.T) {
 		return
 	}
 
-	// var secrets map[string]string
-	// examples.RunExamples(t, "aws", path.Join(cwd, "../examples"), secrets, func(config map[string]string) map[string]string {
-	// 	config["aws:region"] = region
-	// 	return config
-	// })
+	var secrets map[string]string
+	examples.RunExamples(t, "aws", path.Join(cwd, "../examples"), secrets, func(config map[string]string) map[string]string {
+		config["aws:region"] = region
+		return config
+	})
 
 	shortTests := []integration.ProgramTestOptions{
 		{
